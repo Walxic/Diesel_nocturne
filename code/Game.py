5a39172a53608afe4cf811cffba80ba37ba25e87
Walxic <wdp.kms@gmail.com>
2026-05-30 12:02:54 -0300

    back and sound in menu

--- code/Game.py
+++ code/Game.py
@@ -1,23 +1,32 @@
 import pygame
 
-from Code.Menu import Menu
+from code.Menu import Menu
+
+#class Menu:
+#    def __init__(self, window):
+#        self.window = window
+
+#    def run(self):
+#        self.window.fill((0, 0, 0))
+#        pygame.display.flip()
+
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(742, 540))
 
     def run(self):
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
 
-             # chequear todos los eventeos
-             # for event in pygame.event.get():
-             #    if event.type == pygame.QUIT:
-             #        pygame.quit()  # Cerrar la ventana
-             #        quit()  # end pygame
-
+    # chequer todos los events
+    # for event in pygame.event.get():
+    #       if event.type == pygame.QUIT:
+    #          pygame.quit()  # close la ventana
+    #         quit()  # end pygame
--- code/Menu.py
+++ code/Menu.py
@@ -1,8 +1,24 @@
+import pygame
+import pygame.image
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
-        
+        self.surf = pygame.image.load("./asset/Menu_.png")
+        self.rect = self.surf.get_rect()
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.set_volume(0.2)
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            pygame.display.flip()
+            # chequer todos los events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                     pygame.quit()  # close la ventana
+                     quit()  # end pygame
+
+    pass
