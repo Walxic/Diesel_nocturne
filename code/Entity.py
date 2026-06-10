2edfbb34bcde612806a8813b398a4397821462c7
Walxic <wdp.kms@gmail.com>
2026-06-10 20:02:58 -0300

    mediator

--- code/Entity.py
+++ code/Entity.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, ENTITY_HEALTH
 
 
 class Entity(ABC):
@@ -13,6 +13,7 @@ class Entity(ABC):
         self.surf = pygame.image.load('./asset/'+ name + '.png').convert_alpha()
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
+        self.health = ENTITY_HEALTH[self.name]
 
     @abstractmethod
     def move(self, ):
